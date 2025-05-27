382c4ac1457ff89600557214bfe69f11e68fda5b
NathaliaCarriel <nathalia.carriel@escola.pr.gov.br>
2025-05-27 09:32:10 -0300

    Projeto Calculadora

--- js/index.js
+++ js/index.js
@@ -1,4 +1,25 @@
 let peso = document.getElementById(peso);
 let altura = document.getElementById(altura);
+let resultado = document.getElementById(botao);
 let botao = document.getElementById(calcular);
-let resultado = document.getElementById(resultado);
+
+botao.onclick = () => {
+
+  var imc = Number(peso.value) / (Number(altura.value) ** 2);
+  var classificacao = '';
+
+  if( imc < 18.5 ) {
+    classificacao = 'Abaixo do peso';
+  }else if( imc < 25 ) {
+    classificacao = 'Peso normal';
+  }else if( imc < 30 ) {
+    classificacao = 'Sobrepeso';
+  }else if( imc < 35 ) {
+    classificacao = 'Obesidade grau 1';
+  }else if( imc < 40 ) {
+    classificacao = 'Obesidade grau 2';
+  }else {
+    classificacao = 'Obesidade grau 3 (GRAVE)';
+  }
+  resultado.innerText = 'Seu IMC é: ' + imc.toFixed(2) + '\nClassificação: ' + classificacao;
+};
