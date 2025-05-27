ac7e7e7f7de411da23e21f9eab3c285f112b4ccc
NathaliaCarriel <nathalia.carriel@escola.pr.gov.br>
2025-05-27 08:46:27 -0300

    Inicio do Projeto

---
+++ js/index.js
@@ -0,0 +1,4 @@
+let peso = document.getElementById(peso);
+let altura = document.getElementById(altura);
+let botao = document.getElementById(calcular);
+let resultado = document.getElementById(resultado);
